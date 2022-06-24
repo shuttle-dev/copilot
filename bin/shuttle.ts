d0d75af54472f3439fca3766fe3cb565643d42b5
Michael Freund <michael@iamfreund.com>
2022-06-24 09:14:12 +0200

    feat(command): add default command to start environments based on local project configuration

--- bin/shuttle.ts
+++ bin/shuttle.ts
@@ -3,9 +3,11 @@
 import chalk from 'chalk';
 import { Command } from 'commander';
 
+import { DefaultAction } from '../action/default.action';
 import { ListAction } from '../action/list.action';
 import { MenuAction } from '../action/menu.action';
 import { StartAction } from '../action/start.action';
+import { DefaultCommand } from '../command/default.command';
 import { ListCommand } from '../command/list.command';
 import { MenuCommand } from '../command/menu.command';
 import { StartCommand } from '../command/start.command';
@@ -19,6 +21,7 @@ const init = async () => {
 		.usage('<command> [options]')
 		.helpOption('-h, --help', 'Output usage information.');
 
+	new DefaultCommand(new DefaultAction()).load(program);
 	new MenuCommand(new MenuAction()).load(program);
 	new StartCommand(new StartAction()).load(program);
 	new ListCommand(new ListAction()).load(program);
@@ -35,10 +38,6 @@ const init = async () => {
 	});
 
 	program.parse();
-
-	if (!process.argv.slice(2).length) {
-		program.outputHelp();
-	}
 };
 
 init().catch(console.error);
--- lib/util/filesystem.ts
+++ lib/util/filesystem.ts
@@ -1,5 +1,9 @@
 import fs from 'fs';
 
-export const createDirIfNotExists = (directory: string): void => {
+export const ensureDirectoryExists = (directory: string): void => {
 	if (!fs.existsSync(directory)) fs.mkdirSync(directory);
+};
+
+export const ensureFileExists = (file: string): void => {
+	if (!fs.existsSync(file)) throw new Error(`Configuration file not found: ${file}`);
 };
